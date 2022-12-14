1c71a52ee09a4289d812c1814edfeb1d6946b220
paulpaulwoo <woo57@purdue.edu>
2022-12-14 14:29:17 -0500

    carousel prototype

---
+++ frontend/src/components/projectSlide.js
@@ -0,0 +1,78 @@
+import React, {useEffect, useState} from "react"
+import { format } from 'react-string-format';
+import {useSwipeable} from "react-swipeable"
+import './projectSlide.css'
+export const CarouselItem = ({children, width}) => {
+    return (
+        <div className="carousel-item" style={{width: width}}>
+            {children}
+        </div>
+    );
+};
+
+const Carousel = ({children}) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [paused, setPaused] = useState(false)
+    const updateIndex = (newIndex) => {
+        if (newIndex < 0) {
+            newIndex = React.Children.count(children) - 1;
+        } else if (newIndex >= React.Children.count(children)) {
+            newIndex = 0;
+        }
+        setActiveIndex(newIndex)
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!paused){
+            updateIndex(activeIndex + 1);
+            }
+        }, 3000);
+        return () => {
+            if (interval) {
+                clearInterval(interval)
+            }
+        }
+    })
+
+    const handlers = useSwipeable({
+        onSwipedLeft: () => updateIndex(activeIndex + 1),
+        onSwipedRight: () => updateIndex(activeIndex - 1)
+    })
+
+    return (
+        <div className="carousel"
+            onMouseEnter={() => setPaused(true)}
+            onMouseLeave={()=>setPaused(false)}
+            {...handlers}>
+
+            <div className = "inner" style={{ transform: format('translateX(-{0}%)', activeIndex * 100)}} >
+                {React.Children.map(children, (child, index) => {
+                    return React.cloneElement(child, {width:"100%"});
+                })}
+            </div>
+            <div className = "indicators">
+            <button onClick={() => {
+                updateIndex(activeIndex - 1);
+            }}>Prev</button>
+            {React.Children.map(children, (child, index) => {
+                return(
+                    <button
+                        className={format('{0}', index === activeIndex ? "active" : "")}
+                        onClick={() => {
+                            updateIndex(index);
+                        }}
+                        >
+                        {index + 1}
+                    </button>
+                )
+            })}
+            <button onClick={() => {
+                updateIndex(activeIndex + 1);
+            }}>Next</button>
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
