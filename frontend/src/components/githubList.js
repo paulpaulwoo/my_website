e5a9c8d4cff4e673e41b3cadd88fcf2ec8a068ac
paulpaulwoo <woo57@purdue.edu>
2023-01-17 18:32:27 -0500

    fixing text overflow in slider

--- frontend/src/components/githubList.js
+++ frontend/src/components/githubList.js
@@ -5,8 +5,40 @@ import githubCss from './githubList.module.css'
 
 const GithubList = () => {
     const [loaded, setLoaded] = useState(githubCss.unLoaded);
+
+    function apiRequest() {
+        let userInfo = {
+            "id": sessionStorage.getItem("id")
+        };
+        fetch('/getUserPortfolios', {
+            "method": "POST",
+            "headers": {"Content-Type": "application/json"},
+            "body": JSON.stringify(userInfo)
+        }).then(res=>res.json()).then(
+          data => {
+            for (let i = 0; i < data.size; i++) {
+                var port = {
+                    value: data.portIds[i],
+                    label: data.portNames[i]
+                }
+                //portOption.push(port)
+
+            }
+            setLoaded(true)
+          }
+        )
+    }
+
     return (
-        <div className= {githubCss.githubListClass + ' ' + loaded}></div>
+        <div className= {githubCss.githubListClass + ' ' + loaded}>
+            <div className = {githubCss.subject}>
+                I am currently working on....
+            </div>
+        </div>
+
     );
 };
+
+
+
 export default GithubList;
